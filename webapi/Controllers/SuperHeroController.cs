b6b386e905d85d8943bc639fc2aaa07b2a2d6946
mattingram <mingram@vt.edu>
2023-04-05 00:06:54 -0400

    Added http actions for SuperHero api.

--- webapi/Controllers/SuperHeroController.cs
+++ webapi/Controllers/SuperHeroController.cs
@@ -20,5 +20,47 @@ namespace webapi.Controllers
 		{
 			return Ok(await _context.SuperHeros.ToListAsync());
 		}
+
+		[HttpPost]
+		public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+		{
+			_context.SuperHeros.Add(hero);
+			await _context.SaveChangesAsync();
+
+			return Ok(await _context.SuperHeros.ToListAsync());
+		}
+
+		[HttpPut]
+		public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+		{
+			var dbHero = await _context.SuperHeros.FindAsync(hero.Id);
+			if (dbHero == null)
+			{
+				return NotFound();
+			}
+
+			dbHero.Name = hero.Name;
+			dbHero.FirstName = hero.FirstName;
+			dbHero.LastName = hero.LastName;
+			dbHero.Place = hero.Place;
+
+			await _context.SaveChangesAsync();
+
+			return Ok(await _context.SuperHeros.ToListAsync());
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(SuperHero hero)
+		{
+			var dbHero = await _context.SuperHeros.FindAsync(hero.Id);
+			if (dbHero == null)
+			{
+				return NotFound();
+			}
+
+			_context.SuperHeros.Remove(dbHero);
+			await _context.SaveChangesAsync();
+			return Ok(await _context.SuperHeros.ToListAsync());
+		}
 	}
 }
