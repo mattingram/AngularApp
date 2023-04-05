f2ad5e6e26c0550b7f5dd46cce31f8774dc47ff1
mattingram <mingram@vt.edu>
2023-04-04 22:20:53 -0400

    Added SuperHeroController webapi.

---
+++ webapi/Controllers/SuperHeroController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SuperHeroController : Controller
+	{
+		[HttpGet]
+		public async Task<List<SuperHero>> GetSuperHeros()
+		{
+			return new List<SuperHero>()
+			{
+				new SuperHero()
+				{
+					Name = "Spider Man",
+					FirstName = "Peter",
+					LastName = "Parker",
+					Place = "New York"
+				}
+			};
+		}
+	}
+}
